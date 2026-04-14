8a9c26940e6686f76a2488c10d5fb7e10ff5130d
WillianBoita <wkboita@gmail.com>
2026-04-14 16:00:31 -0300

    alerado

--- data.c
+++ data.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "data.h"
 
 int main() {
   Data d1, d2;
+  int diasDoMes[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-  setDate(&d1, 30, 11, 2000);
-  setDate(&d2, 30, 11, 1987);
+  setDate(&d1, 28, 2, 2000, diasDoMes);
+  setDate(&d2, 31, 12, 1988, diasDoMes);
 
-  getDate(d1, 'Y');
+  //getDate(d1, 'Y');
 
-  showDate(d1);
+  //showDate(d1);
 
-  printf("%d\n", dataComp(d1, d2));
+  //printf("Comparar data: %d\n", dataComp(d1, d2));
 
-  printf("%d\n", isLeapYear(d1));
+  //printf("Ano bissexto: %d\n", isLeapYear(d1.ano));
+
+  //printf("Diferença em dias: %d\n", getDiff(d1, d2));
+
+  printf("dias do ano: %d\n", getDayFromYear(d2, diasDoMes));
 
   return 0;
 }
 
-void setDate(Data *data, int dia, int mes, int ano) {
-  if(!dia || !mes || !ano){
+void setDate(Data *data, int dia, int mes, int ano, int dias[12]) {
+  int copiaDias[12];
+
+  for (int i = 0; i < 12; i++) {
+        copiaDias[i] = dias[i];
+    }
+
+  if(isLeapYear(ano)) {
+    copiaDias[1] += 1;
+  }
+
+  if(!dia || (!mes || mes > 12) || !ano || dia > copiaDias[mes - 1]){
     printf("Dados inválidos\n");
     return;
   }
@@ -30,15 +46,15 @@ void setDate(Data *data, int dia, int mes, int ano) {
 };
 
 int getDate(Data data, char opcao) {
-  if (opcao == 'Y' || opcao == 'y' && data.ano) {
+  if ((opcao == 'Y' || opcao == 'y') && data.ano) {
     printf("%d\n", data.ano);
     return data.ano;
 
-  } else if (opcao == 'M' || opcao == 'm' && data.mes) {
+  } else if ((opcao == 'M' || opcao == 'm') && data.mes) {
     printf("%d\n", data.mes);
     return data.mes;
 
-  } else if (opcao == 'D' || opcao == 'd' && data.dia) {
+  } else if ((opcao == 'D' || opcao == 'd') && data.dia) {
     printf("%d\n", data.dia);
     return data.dia;
 
@@ -72,21 +88,45 @@ int dataComp(Data data1, Data data2) {
 };
 
 int getDiff(Data data1, Data data2) {
-
+  int diff = 0;
+  if (data1.ano == data2.ano && data1.mes == data2.mes) {
+    diff = abs(data1.dia - data2.dia);
+  } else {
+    
+  }
+  
+  return diff;
 };
 
-int isLeapYear(Data data) {
-  if (data.ano % 4 == 0) {
-    if (data.ano % 100 == 0) {
-      if (data.ano % 400 == 0) {
+int isLeapYear(int data) {
+  if (data % 4 == 0) {
+    if (data % 100 == 0) {
+      if (data % 400 == 0) {
         return 1;
       }
       return 0;
     }
     return 1;
   }
+
+  return 0;
 };
 
-int getDayFromYear(Data data) {
+int getDayFromYear(Data data, int dias[12]) {
+  int diasAcumulados = 0;
+  printf("fevereiro: %d\n", dias[1]);
 
+  for (int i = 0; i < data.mes; i++) {
+    printf("%d mes: %d\n", i, dias[i]);
+    if (i == data.mes - 1) {
+      diasAcumulados += data.dia;
+      printf("dia mes inicial: %d\n", data.dia);
+      continue;
+    }
+
+    diasAcumulados += dias[i];
+  }
+  
+
+  return diasAcumulados;
 };
--- data.h
+++ data.h
@@ -4,7 +4,7 @@ typedef struct data {
   int ano;
 } Data;
 
-void setDate(Data *data, int dia, int mes, int ano);
+void setDate(Data *data, int dia, int mes, int ano, int dias[12]);
 
 int getDate(Data data, char opcao);
 
@@ -14,6 +14,6 @@ int dataComp(Data data1, Data data2);
 
 int getDiff(Data data1, Data data2);
 
-int isLeapYear(Data data);
+int isLeapYear(int data);
 
-int getDayFromYear(Data data);
+int getDayFromYear(Data data, int dias[12]);
